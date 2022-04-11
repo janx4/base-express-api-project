716f526bce6f29a96705a8e39e2e2801933c9281
janx4 <huytruong641@gmail.com>
2022-04-11 23:13:58 +0700

    feat: adding 404 page for 404 HTTP status, using CHALK.js to decorate the message on console

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const chalk = require("chalk");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5678;
+const corsOptions = {
+    origin: "*",
+};
 
 // Security configuration
-app.use(cors());
+app.disable('x-powered-by');
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(
@@ -19,18 +25,31 @@ app.use(
 
 // API Routing
 app.use("/api", require("./src/routes/router"));
+
+// Default response -> HTTP_NOT_FOUND 404 & render 404.html page
 app.use((req, res) => {
-    return res.status(404).send();
+    return res
+        .status(404)
+        .sendFile(path.join(__dirname, "/src/views/errors/404.html"));
 });
 
 // Errors handler
 app.use((err, req, res, next) => {
-    console.log(err);
-    return res.status(500).send();
+    console.log(chalk.bgRed(err));
+
+    // Default error handler
+    return res.status(500).send({
+        error: "An error has occurred. Please try again later.",
+    });
 });
 
-// You can check the database connection before run the server
+/**
+ * You can check the database connection before run the server
+ * Using CHALK to display the error message, easy to read and debug.
+ */
 app.listen(port, () => {
-    console.log(`Backend server is listening on port ${port}`);
-    console.log(`http://localhost:${port}`);
+    console.log(
+        chalk.bgYellow.black.bold(`Backend server is listening on port ${port}...`)
+    );
+    console.log(chalk.bold.blue(`http://localhost:${port}`));
 });
